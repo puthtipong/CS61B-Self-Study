923d5156881cbb86692d02fb653626e2b2699b6a
puthtipong <puthtipong.t@gmail.com>
2022-06-12 22:13:03 +0700

    updated lab1 details

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,7 +8,7 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year){
         /**Accepts int input and calculates if it is divisible by 400 or by 4 and not 100, in which case it is a leap year and returns true. Vice versa */
-        if(year%400==0 || year%4==0 && year%100!=0){
+        if(year%400==0 || (year%4==0 && year%100!=0)){
             return true;
         }
         return false;
